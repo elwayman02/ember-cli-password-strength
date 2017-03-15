2933fce97ef15cec633d20ee78e51408d279fd68
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-03-15 09:16:58 -0700

    Get zxcvbn from NPM instead of bower
    Much more convenient for addon consumers as they don't have to manage
    this dependency.

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
   name: 'ember-cli-password-strength',
 
-  included: function (app) {
+  included(app) {
     this._super.included.apply(this, arguments);
 
     if (typeof app.import !== 'function' && app.app) {
       app = app.app;
     }
-
-    app.import(app.bowerDirectory + '/zxcvbn/dist/zxcvbn.js');
+    app.import('vendor/zxcvbn.js');
     app.import('vendor/shims/password-strength.js');
-  }
+  },
+
+  treeForVendor(vendorTree) {
+    var zxcvbnTree = new Funnel(path.dirname(require.resolve('zxcvbn/dist/zxcvbn.js')), {
+      files: ['zxcvbn.js'],
+    });
+
+    return new MergeTrees([vendorTree, zxcvbnTree]);
+  },
 };
