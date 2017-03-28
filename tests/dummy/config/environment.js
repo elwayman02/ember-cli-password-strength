76131ee8dad9b7aa81e74a05dbf95639e9c1e749
Jordan Hawker <hawker.jordan@gmail.com>
2017-03-28 16:39:06 -0700

    Upgrade Ember 2.12 via `ember init`

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,4 +1,4 @@
-/* jshint node: true */
+/* eslint-env node */
 
 module.exports = function(environment) {
   var ENV = {
@@ -43,8 +43,6 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.locationType = 'hash';
-    ENV.rootURL = '/ember-cli-password-strength/';
 
   }
 
